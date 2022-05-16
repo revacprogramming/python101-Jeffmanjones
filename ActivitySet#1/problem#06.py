7e3614b231757c1f0e0b58ca6542bd2f4fab4d1f
Jeffrey Immanuel <94883101+Jeffmanjones@users.noreply.github.com>
2022-05-16 00:56:26 +0000

    update

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -5,13 +5,13 @@ smallest = None
 
 while True:
     num = input("Enter a number? ")
-    if num == "done":
+    try if num == "done":
         break
     if num>largest:
         largest=num
     if num<smallest:
         smallest=num
-    print(num)
+except print(num)
 
 print("Maximum", largest)
 print("Minimum", smallest)
