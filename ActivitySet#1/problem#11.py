61c12139480993bd4d10719a6e3d97250547004e
Jeffrey Immanuel <94883101+Jeffmanjones@users.noreply.github.com>
2022-05-21 08:05:53 +0000

    update

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,19 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+handle = open(name)
+hours={}
+for line in handle:
+	if "From:" in line:
+         continue
+	elif "From" in line:
+    	 time=line.split()
+    	 time=str(time[5]).split(":")
+    	 if time[0] not in hours:
+             hours[time[0]]=1
+         else:
+             hours[time[0]]=hours.get(time[0],0) + 1
+                    
+for i,j in sorted(hours.items()):
+	print(i,j)
