3103aac83267237f66c11ae97a02bb352db0a66b
Jeffrey Immanuel <94883101+Jeffmanjones@users.noreply.github.com>
2022-07-08 03:25:53 +0000

    update

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -2,8 +2,8 @@ def add(a,b):
   sum=a+b
   return sum
 
-a = float(input("Enter a number"))
-b = float(input("Enter a number"))
+a = float(input("Enter a number "))
+b = float(input("Enter a number "))
 
 c = add(a,b)
 print ("The sum is",c)
