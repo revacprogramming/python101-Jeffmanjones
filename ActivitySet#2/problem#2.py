e4bce7e7f02a02a1c3f5391a45774fec1752f9db
Jeffrey Immanuel <94883101+Jeffmanjones@users.noreply.github.com>
2022-07-08 03:54:53 +0000

    update

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,18 +1,17 @@
 
 def add(a, b):
-    sum=a+b
-    return sum
     pass  
 
 def output(a, b, sum):
-    print("The sume of ",a," and ",b," is ",sum)
     pass  
 
 def main():
     a=float(input("Enter a number: ")) 
     b=float(input("Enter a number: "))
-    sum = add(a, b)
+    sum=a+b
+    add(a, b)
     output(a, b, sum)
+    print("The sum of ",a," and ",b," is ",sum)
 
 if __name__ == '__main__':
     main()
