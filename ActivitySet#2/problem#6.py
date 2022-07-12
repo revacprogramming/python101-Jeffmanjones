f7b58979ba052d752f7ffab020a70303f22617f1
Jeffrey Immanuel <94883101+Jeffmanjones@users.noreply.github.com>
2022-07-12 10:40:50 +0000

    update

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,13 +1,12 @@
-
-
 class Menu:
-    #fill in class definition here
-    def __init__(self):
-      self.add=add
-    
-
+  #fill in class definition here
+  def __init__(self,item,price):
+    self.item=item
+    self.price=price
+    def menu()
+      d=dict()
+      
 m = Menu()  # Menu is a class
 m.add("idly", 10)
 m.add("vada". 20)
-
 m.show()
