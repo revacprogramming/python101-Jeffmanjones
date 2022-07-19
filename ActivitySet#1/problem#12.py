f0002d2665964a275daef5d1114102901e640595
Jeffrey Immanuel <94883101+Jeffmanjones@users.noreply.github.com>
2022-07-19 10:46:38 +0000

    update

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -4,9 +4,9 @@ fname = input("Enter file:")
 if len(fname) < 1 : fname="regex_sum_42.txt"
 handle = open(fname)
 for line in handle:
-    numbers = re.findall("[0-9]+",line)
-    if not numbers: continue
-    else:         
-        for num in numbers:
-            total = total + int(num)
+  numbers = re.findall("[0-9]+",line)
+  if not numbers: continue
+  else:        
+    for num in numbers:
+      total = total + int(num)
 print(total)
--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,15 +1,24 @@
+d=dict()
 class Menu:
   #fill in class definition here
   def __init__(self,item,price):
     self.item=item
     self.price=price
-    self.d={}
+    
   def add(self,item,price):
-    
-  def show(self,item,price):
-    
-      
-m = Menu()  # Menu is a class
-m.add("idly", 10)
-m.add("vada", 20)
+    d[self.item]=self.price
+  def show(self):
+    print(d)
+
+n=int(input("Number of items in the menu:\n"))
+i=0
+while i<n:
+  item=input("Enter an item: ")
+  price=float(input("Enter the price: "))
+  m=Menu(item,price)
+  m.add(item,price)
+  i=i+1
 m.show()
+
+#m.add("idly", 10) 
+#m.add("vada", 20)
