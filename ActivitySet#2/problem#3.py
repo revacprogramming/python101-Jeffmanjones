054e6ed95f6ea43e7930d9ea54ee3a9d12c625bf
Jeffrey Immanuel <94883101+Jeffmanjones@users.noreply.github.com>
2022-07-19 09:24:43 +0000

    update

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -3,11 +3,14 @@ def get_cs():
     return word
 
 def cs_to_lot(cs):
-
+    lst=cs.split()
+    return lst
+  
 def main():
     cs = get_cs()
     lot = cs_to_lot(cs)
     print(lot)
+  
 if __name__ == '__main__':
     main()
 
