27a500d44c8bfb36868f908ff2d6a8d12cf83650
Jeffrey Immanuel <94883101+Jeffmanjones@users.noreply.github.com>
2022-07-23 15:00:30 +0000

    update

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -9,8 +9,7 @@ def cs_to_dict(cs):
 
 def dict_to_cs(d):
     #convert a dictionary to connect string
-    cs=
-    return cs
+    =
     pass
   
 def main():
--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,8 +1,11 @@
-
-
 class Menu:
-    """fill in class definition here"""
-
+  """fill in class definition here"""
+  def __init__(self,item,price):
+    self.item=item
+    self.price=price
+  
+  def __add__(self,item,price):
+    
 
 m = Menu()
 m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
